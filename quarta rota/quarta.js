020d9d120265a25215a2a01ca5d9f723aa5b4ff8
Marcelo Mont-Morency Gondim Rodrigues <morencymarcelo@gmail.com>
2022-11-15 20:18:23 -0300

    marcelo 5 rotas

---
+++ quarta rota/quarta.js
@@ -0,0 +1,30 @@
+const express =  require("express");
+const app = express();
+
+app.get("/",function(req,res){
+    console.log(__dirname)
+  res .sendFile(__dirname + "/segunda.html")
+});
+
+app.get("/tvs",function(req,res){
+  console.log(__dirname)
+  res .sendFile(__dirname + "/tv.html")
+  });
+
+app.get("/smartphones",function(req,res){
+  console.log(__dirname)
+  res .sendFile(__dirname + "/smartphones.html")
+});
+
+  app.get("/moda",function(req,res){
+    console.log(__dirname)
+    res .sendFile(__dirname + "/tv.html")
+  });
+    app.get("/pesquisa",function(req,res){
+      console.log(__dirname)
+      res .sendFile(__dirname + "/tv.html")
+    });
+  app.listen(8084,function(){console.log("servidor rodando");});
+
+
+module.exports = app;

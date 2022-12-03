62d558fc1e810d1381cfe8b61dbaf849b3079ecb
Wilson Ferreira <willneto@edu.unifor.br>
2022-12-03 11:31:34 -0300

    refatorando codigo.

--- mercadoFechado.js
+++ mercadoFechado.js
@@ -1,64 +1,60 @@
-const express =  require("express");
+const express = require("express");
 const app = express();
+const path = require("path");
 
-  app.get("/", function(req,res){
-    console.log(__dirname)
-    res.sendFile(__dirname + "/login.html");
-  });
+function upRender(fileName) {
+  return path.join(__dirname, "views", fileName + ".html");
+}
 
-  app.get("/tvs",function(req,res){
-    console.log(__dirname)
-    res .sendFile(__dirname + "/tv.html")
-  });
+app.get("/", function (req, res) {
+  res.sendFile(upRender("login"));
+});
 
-  app.get("/smartphones",function(req,res){
-    console.log(__dirname)
-    res .sendFile(__dirname + "/smartphones.html")
-  });
+app.get("/smartphones", function (req, res) {
+  res.sendFile(upRender("smartphones"));
+});
 
-  app.get("/moda",function(req,res){
-    console.log(__dirname)
-    res .sendFile(__dirname + "/moda.html")
-  });
+app.get("/moda", function (req, res) {
+  res.sendFile(upRender("moda"));
+});
 
-  app.get("/carrinho",function(req,res){
-    console.log(__dirname)
-    res .sendFile(__dirname + "/carrinho.html")
-  });
+app.get("/carrinho", function (req, res) {
+  res.sendFile(upRender("carrinho"));
+});
 
-  app.get("/ofertas",function(req,res){
-    console.log(__dirname)
-    res .sendFile(__dirname + "/paginaOfertas.html")
-  });
+app.get("/ofertas", function (req, res) {
+  res.sendFile(upRender("paginaOfertas"));
+});
 
-  app.get("/suporte",function(req,res){
-    console.log(__dirname)
-    res .sendFile(__dirname + "/suporte.html")
-  });
+app.get("/suporte", function (req, res) {
+  res.sendFile(upRender("suporte"));
+});
 
-  app.get("/inicio",function(req,res){
-    console.log(__dirname)
-    res .sendFile(__dirname + "/inicio.html")
-  });
+app.get("/inicio", function (req, res) {
+  res.sendFile(upRender("inicio"));
+});
 
-  app.get("/tv",function(req,res){
-    console.log(__dirname)
-    res .sendFile(__dirname + "/tv.html")
-  });
-  app.get("/tvs/:marca",function(req,res){
-    switch(req.params.marca) {
-    case 'samsung':
-    res.redirect('https://www.casasbahia.com.br/smart-tv-50-uhd-4k-samsung-50au7700-processador-crystal-4k-tela-sem-limites-visual-livre-de-cabos-alexa-built-in-controle-unico-55021049/p/55021049?utm_medium=Cpc&utm_source=GP_PLA&IdSku=55021049&idLojista=10037&tipoLojista=1P&&utm_campaign=gg_pmax_core_elte_apostas&gclid=EAIaIQobChMIrbTk5tnZ-wIVCjORCh1evgDDEAQYASABEgJyu_D_BwE&gclsrc=aw.ds');
-    break;
-    case 'lg':
-    res.redirect('https://www.casasbahia.com.br/smart-tv-50-lg-4k-uhd-50uq8050-wifi-bluetooth-hdr-nvidia-geforce-now-thinq-ai-smart-magic-google-alexa-55051821/p/55051821?utm_medium=Cpc&utm_source=GP_PLA&IdSku=55051821&idLojista=10037&tipoLojista=1P&&utm_campaign=gg_pmax_core_elte_apostas&gclid=EAIaIQobChMI3ITWgNrZ-wIVSRZMCh21JgUZEAQYASABEgKDN_D_BwE&gclsrc=aw.ds');
-    break;
-    }
-    });
-    
-    
+app.get("/tvs", function (req, res) {
+  res.sendFile(upRender("tvs"));
+});
 
-  app.listen(8081,function(){console.log("servidor rodando");});
+app.get("/tvs/:marca", function (req, res) {
+  switch (req.params.marca) {
+    case "samsung":
+      res.redirect(
+        "https://www.casasbahia.com.br/smart-tv-50-uhd-4k-samsung-50au7700-processador-crystal-4k-tela-sem-limites-visual-livre-de-cabos-alexa-built-in-controle-unico-55021049/p/55021049?utm_medium=Cpc&utm_source=GP_PLA&IdSku=55021049&idLojista=10037&tipoLojista=1P&&utm_campaign=gg_pmax_core_elte_apostas&gclid=EAIaIQobChMIrbTk5tnZ-wIVCjORCh1evgDDEAQYASABEgJyu_D_BwE&gclsrc=aw.ds"
+      );
+      break;
+    case "lg":
+      res.redirect(
+        "https://www.casasbahia.com.br/smart-tv-50-lg-4k-uhd-50uq8050-wifi-bluetooth-hdr-nvidia-geforce-now-thinq-ai-smart-magic-google-alexa-55051821/p/55051821?utm_medium=Cpc&utm_source=GP_PLA&IdSku=55051821&idLojista=10037&tipoLojista=1P&&utm_campaign=gg_pmax_core_elte_apostas&gclid=EAIaIQobChMI3ITWgNrZ-wIVSRZMCh21JgUZEAQYASABEgKDN_D_BwE&gclsrc=aw.ds"
+      );
+      break;
+  }
+});
 
+app.listen(8081, function () {
+  console.log("servidor rodando");
+});
 
 module.exports = app;

69a6d3f35cff387413687db8131752e2b4277723
marcelo mont-morency <morencymarcelo@gmail.com>
2022-12-03 10:56:02 -0300

    enviando rota

--- mercadoFechado.js
+++ mercadoFechado.js
@@ -45,7 +45,18 @@ const app = express();
     console.log(__dirname)
     res .sendFile(__dirname + "/tv.html")
   });
-
+  app.get("/tvs/:marca",function(req,res){
+    switch(req.params.marca) {
+    case 'samsung':
+    res.redirect('https://www.casasbahia.com.br/smart-tv-50-uhd-4k-samsung-50au7700-processador-crystal-4k-tela-sem-limites-visual-livre-de-cabos-alexa-built-in-controle-unico-55021049/p/55021049?utm_medium=Cpc&utm_source=GP_PLA&IdSku=55021049&idLojista=10037&tipoLojista=1P&&utm_campaign=gg_pmax_core_elte_apostas&gclid=EAIaIQobChMIrbTk5tnZ-wIVCjORCh1evgDDEAQYASABEgJyu_D_BwE&gclsrc=aw.ds');
+    break;
+    case 'lg':
+    res.redirect('https://www.casasbahia.com.br/smart-tv-50-lg-4k-uhd-50uq8050-wifi-bluetooth-hdr-nvidia-geforce-now-thinq-ai-smart-magic-google-alexa-55051821/p/55051821?utm_medium=Cpc&utm_source=GP_PLA&IdSku=55051821&idLojista=10037&tipoLojista=1P&&utm_campaign=gg_pmax_core_elte_apostas&gclid=EAIaIQobChMI3ITWgNrZ-wIVSRZMCh21JgUZEAQYASABEgKDN_D_BwE&gclsrc=aw.ds');
+    break;
+    }
+    });
+    
+    
 
   app.listen(8081,function(){console.log("servidor rodando");});
 
